c94dbf6408d352215d982119e692d3d5b69dd36a
KCSquid <jahvonc9@gmail.com>
2025-03-27 16:18:20 -0400

    added font and started programming

--- app/programming/programming.tsx
+++ app/programming/programming.tsx
@@ -3,45 +3,37 @@ import logoLight from "./logo-light.svg";
 
 export function Programming() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              wow
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
+    <main className="flex items-center justify-center pt-16 pb-4 font-[Inter]">
+      <div className="flex flex-col">
+        <h1 className="uppercase text-4xl font-[Passion_One]">
+          The Programming Team
+        </h1>
+        <div className="flex flex-col gap-2">
+          <p className="font-medium">
+            We, programming team are responsible for bringing life to the robot
+            we make use of a variety of tools and technologies in order to
+            efficiently and accurately address challenges our robot has to face.
+            Some of the things we do include:
+          </p>
+          <ul className="list-disc pl-5">
+            <li>
+              Simulation testing - testing the robot in a virtual environment
+            </li>
+            <li>
+              PID curve tuning - tuning the motor settings in order to smoothly
+              start and stop
+            </li>
+            <li>
+              Graphics programming - using linear algebra to translate raw data
+            </li>
+            <li>
+              AI vision - using algorithms to assess the position of objects and
+              more
+            </li>
+          </ul>
         </div>
       </div>
+      <img src="" alt="" />
     </main>
   );
 }
