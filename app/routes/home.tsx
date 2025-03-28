7b993440a763934f3f32d73f1ee62756ccc2b813
thesamyatgautam <thesamyatgautam@gmail.com>
2025-03-28 11:08:41 -0400

    title page for large screens finished

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import { useState } from "react";
 import Navbar from "../../src/components/Navbar"
 
 export function meta({}: Route.MetaArgs) {
@@ -8,11 +9,47 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+function MainText() {
+  return (
+    <article className="relative">
+      <div className="w-14 bg-blue-400 h-1 rounded mb-3" />
+      <h1 className="font-[Passion_One] text-3xl mb-1">FRC TEAM 3560 MECHAWOLVES</h1>
+      <p className="mb-6">Ready to build the future? Join our robotics club and dive into hands-on fun with robots! Registrations are now open for grade 9 - 12's. Work on cool projects, learn new skills, and compete in exciting challenges. Don’t wait—sign up today and start your robotics adventure!</p>
+      <button className="rounded-sm px-4 py-2 border-1 border-white text-xl font-[Passion_One]">Contact Us</button>
+    </article>
+  )
+}
+
 export default function Home() {
+  const displays = [
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" },
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" },
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" },
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" },
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" },
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" },
+    { image: "https://3560.ca/wp-content/uploads/2024/09/6.png" }
+  ];
+  
+  const [currentDisplay, setCurrentDisplay] = useState(0);
+
   return (
     <>
-      <div>
+      <div className="w-screen lg:px-[145px] px-[45px] py-10">
         <Navbar />
+        <section className="relative h-[700px] w-screen mt-[-80px] lg:ml-[-145px] ml-[-45px] overflow-x-clip">
+          {displays[currentDisplay].image && <img className="w-full object-center object-cover h-full absolute" src={displays[currentDisplay].image} />}
+          <div className="bg-black opacity-85 w-full h-full absolute"></div>
+          <div className="px-[45px] lg:px-[145px] h-full flex items-center lg:w-2/3">
+            <MainText />
+          </div>
+          <div className="b-[145px] absolute bottom-5 -translate-y-1/2 left-1/2 -translate-x-1/2 flex gap-3">
+            {displays.map((d, i) => (
+              <div className={"rounded-full w-2 h-2 " + (i == currentDisplay ? "bg-blue-400" : "bg-blue-200")}></div>
+            ))}
+          </div>
+          <div className="font-[Passion_One] text-blue-950 text-lg absolute bottom-0 translate-y-1/2 -translate-x-1/2 left-1/2">MECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVESMECHAWOLVES</div>
+        </section>
       </div>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,44 @@
+import DropdownArrow from "../../public/DropdownArrow.svg"
+import DropdownArrowUnfilled from "../../public/DropdownArrowUnfilled.svg"
+import { useState } from "react"
+
+function NavDropdown(props) {
+  const options = props.options;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="font-[Passion_One] relative" {...props} onClick={() => {
+      setOpen((open) => !open)
+    }}>
+      <span className="cursor-pointer flex gap-1">
+        {props.name} <img className="-mt-1" src={open ? DropdownArrow : DropdownArrowUnfilled} />
+      </span>
+      {open && <div className="absolute bg-black/40 py-2 w-30 rounded -translate-x-1/2 gap-3 left-1/2 flex flex-col">
+        {
+        options.map((option) => (
+          <a className="hover:bg-white/10 rounded px-1.5 -my-2 py-0.5" href={option.link}>{option.name}</a>
+        ))
+        }
+      </div>}
+    </nav>
+  )
+}
+
 export default function Navbar() {
   return (
-    <div class="flex gap-4">
+    <div className="flex gap-4 justify-end w-full z-10 relative sticky">
       {
         [{
-          name: "SUBTITLES",
-          link: "https://"
+          name: "SUBTEAMS",
+          link: "dropdown",
+          options: [
+            { name: "Mechanical", link: "https://" },
+            { name: "CAD", link: "https://" },
+            { name: "Programming", link: "https://" },
+            { name: "Electrical", link: "https://" },
+            { name: "Buisiness", link: "https://" },
+            { name: "Graphic Design", link: "https://" },
+          ]
         },{
           name: "REGISTRATION",
           link: "https://"
@@ -24,8 +58,8 @@ export default function Navbar() {
           name: "CONTACT US",
           link: "https://"
         },
-        ].map((link) => (
-          <a href={link.link}>{link.name}</a>
+        ].map((link) => link.link === "dropdown" ? (<NavDropdown name={link.name} options={link.options}/>) : (
+          <a href={link.link} className="font-[Passion_One]">{link.name}</a>
         ))
       }
     </div>
