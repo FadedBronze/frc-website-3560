51d12a7449865c34c4690472467a5de42fbd7aa5
thesamyatgautam <thesamyatgautam@gmail.com>
2025-05-23 19:39:28 -0400

    fixed map icon fs now

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,29 @@
 import "leaflet/dist/leaflet.css";
 import { useRef, useEffect } from "react";
-import L from "leaflet";
+import L, {icon, Marker} from "leaflet";
 
 export default function Map() {
   const initRef = useRef(false);
 
   useEffect(() => {
     if (initRef.current == false) {
+      const iconRetinaUrl = '/marker-icon-2x.png';
+      const iconUrl = '/marker-icon.png';
+      const shadowUrl = '/marker-shadow.png';
+
+      const iconDefault = icon({
+        iconRetinaUrl,
+        iconUrl,
+        shadowUrl,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowSize: [41, 41]
+      });
+
+      Marker.prototype.options.icon = iconDefault;
+
       let map = L.map('map').setView([43.738041, -79.727204], 17);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
